bc46b45d9b303dfbd8a126daf3cc58e29a94a40e
tubaShawn <shawnrmangum@gmail.com>
2018-08-04 11:21:24 -0600

    working on fb login

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
+var passport = require("passport-facebook");
 
 var db = require("./models");
 
@@ -12,6 +13,19 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
+
+// FB login
+passport.use(new FacebookStrategy({
+  clientID: "537870203312035",
+  clientSecret: "b23a4901e1fdf032e11094eb4f8a375a",
+  callbackURL: "http://localhost:3000/auth/facebook/callback"
+},
+function(accessToken, refreshToken, profile, cb) {
+  User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+    return cb(err, user);
+  });
+}
+));
 
 // Handlebars
 app.engine(
@@ -25,6 +39,7 @@ app.set("view engine", "handlebars");
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+
 
 var syncOptions = { force: false };
 
